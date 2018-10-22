05ce51f1723e035c67871059116c3aaf5ea99b2a
Jesus Palos <jesus.rpalos@gmail.com>
2018-10-22 00:14:29 -0500

    fixing about

--- src/Components/Presentation/AboutContainer.js
+++ src/Components/Presentation/AboutContainer.js
@@ -5,33 +5,6 @@ const ip = "http://dateapi.sabrinaherrero.com";
 
 export default class AboutContainer extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            dates: [],
-            loaded: false
-        }
-    }
-
-    componentDidMount()
-    {
-        let objects = [];
-        fetch(`${ip}/date`).then(results => {
-            return results.json();
-        })
-            .then(data => {
-                    let numObjects = data.num_results;
-                    for(let i = 0; i < numObjects; i++){
-                        objects.push(data.objects[i]);
-                    }
-                    this.setState({
-                        dates: objects,
-                        loaded: true
-                    })
-                }
-            )
-    }
-
     render() {
         return (
             <div>
